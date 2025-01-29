724ef7d734a1338b12fed233fd4a1811a4077ce7
Adam-S988 <adam.stevenson@keyin.com>
2025-01-29 14:25:22 -0330

    Creation of a Sample Test

--- src/test/java/GameStoreTest.java
+++ src/test/java/GameStoreTest.java
@@ -1,3 +1,12 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class GameStoreTest {
 
+    @Test
+    public void gamePriceTest() {
+        int a = 1;
+        int b = 2;
+        Assertions.assertTrue(a < b);
+    }
 }
