7a3d0f9c8cd6d68cb73b46ae6bbd809831d258d4
Adam-S988 <adam.stevenson@keyin.com>
2025-01-29 14:17:00 -0330

    First commit

--- src/main/java/com/keyin/Game.java
+++ src/main/java/com/keyin/Game.java
@@ -1,4 +1,37 @@
 package com.keyin;
 
 public class Game {
+    private int gameId;
+    private String gameTitle;
+    private double gamePrice;
+
+    public Game(int gameId, String gameTitle, double gamePrice) {
+        this.gameId = gameId;
+        this.gameTitle = gameTitle;
+        this.gamePrice = gamePrice;
+    }
+
+    public int getGameId() {
+        return gameId;
+    }
+
+    public void setGameId(int gameId) {
+        this.gameId = gameId;
+    }
+
+    public String getGameTitle() {
+        return gameTitle;
+    }
+
+    public void setGameTitle(String gameTitle) {
+        this.gameTitle = gameTitle;
+    }
+
+    public double getGamePrice() {
+        return gamePrice;
+    }
+
+    public void setGamePrice(double gamePrice) {
+        this.gamePrice = gamePrice;
+    }
 }
